2e77fe3d43113a77f68f876016ae8d7754877f34
kimyoungmin <yhoosung@naver.com>
2019-02-16 17:57:19 +0900

    -----------Data Merge Test------------- 	DATA  : 2019 02 16 	TITLE : Source Modify --------------------------------------

--- testsrc/Derive.cpp
+++ testsrc/Derive.cpp
@@ -4,10 +4,13 @@
 CDerive::CDerive(const char* msg):CBase(msg)
 {
 	std::cout << "CDerive 생성자 호출" << std::endl;
+	std::cout << "insert src" << std::endl;
 }
 
 
 CDerive::~CDerive(void)
 {
 	std::cout << "CDerive 소멸자 호출" << std::endl;
+	std::cout << "insert src" << std::endl;
 }
+
