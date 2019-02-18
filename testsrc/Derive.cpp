cc30677a9438630917edf62f1cee4ea0eee3f9c6
kimyoungmin <yhoosung@naver.com>
2019-02-18 09:12:13 +0900

    ---------------------------- 	DATA : 2019 02 18 	TITLE: GUI ISSUE ----------------------------

--- testsrc/Derive.cpp
+++ testsrc/Derive.cpp
@@ -4,11 +4,13 @@
 CDerive::CDerive(const char* msg):CBase(msg)
 {
 	std::cout << "CDerive 생성자 호출" << std::endl;
+	std::cout << "CDerive Insert Data" << std::endl;
 }
 
 
 CDerive::~CDerive(void)
 {
 	std::cout << "CDerive 소멸자 호출" << std::endl;
+	std::cout << "~CDerive Insert Data" << std::endl;
 }
 
